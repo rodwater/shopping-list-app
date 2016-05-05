bd251988b59f5a05ad4392bf6a236b7c182afadb
Rodwater <rodwater@gmail.com>
2016-05-05 07:13:21 -0700

    final v2

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
             '<img src="images/tick.png" width="13px"height="13px" id="tick">' + " " + '<img src="images/delete.png" width="13px"height="13px" class="remove_field">' + "</li>");
     });
     $(document).on("click", "#tick", function (e) {
-        e.preventDefault(); $(this).parent('li').css('opacity' ,'.3').css('font-size' , '.8em').css('font-style', 'italic').css('text-decoration' , 'line-through');
+        e.preventDefault(); $(this).parent('li').css('opacity' ,'.3').css('font-style', 'italic').css('text-decoration' , 'line-through');
     });
     $(document).on("click",".remove_field", function(e){
         e.preventDefault(); $(this).parent('li').remove();
